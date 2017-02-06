8d5d0317a746d43e61b51b87c55768617f9fc54f
Rainer Wittmann <rainer.wittmann@comsysto.com>
2017-02-06 23:59:37 +0100

    implement splash screen

--- lib/components/todo_app_scaffold.dart
+++ lib/components/todo_app_scaffold.dart
@@ -1,6 +1,8 @@
+import 'dart:async';
 import 'package:ToDo/components/todo_app_body.dart';
 import 'package:ToDo/components/todo_app_create.dart';
 import 'package:ToDo/components/todo_app_drawer.dart';
+import 'package:ToDo/components/todo_app_splash.dart';
 import 'package:flutter/material.dart';
 
 class ToDoAppScaffold extends StatefulWidget {
@@ -41,6 +43,16 @@ class ToDoAppScaffold extends StatefulWidget {
 }
 
 class ToDoAppScaffoldState extends State<ToDoAppScaffold> {
+  bool _displaySplash = true;
+
+  void _removeSplash() {
+    new Timer(const Duration(milliseconds: 4000), () {
+      setState(() {
+        _displaySplash = false;
+      });
+    });
+  }
+
   Widget _getAppTitle() {
     String title;
     switch (config.indicator) {
@@ -59,39 +71,45 @@ class ToDoAppScaffoldState extends State<ToDoAppScaffold> {
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-        drawer: new ToDoAppDrawer(
-            config.indicator,
-            config.color,
-            config.brightness,
-            config.changeTheme,
-            config.changeColor,
-            config.emptyTrash),
-        appBar: new AppBar(
-          elevation: 2,
-          title: _getAppTitle(),
-          actions: [
-            config.indicator == 0
-                ? new IconButton(
-                    icon: new Icon(Icons.create),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        new MaterialPageRoute(builder: (BuildContext context) {
-                          return new ToDoAppCreate(config.createToDo);
-                        }),
-                      );
-                    })
-                : new Container(),
-          ],
-        ),
-        body: new ToDoAppBody(
-            config.indicator,
-            config.toDoList,
-            config.toggleDone,
-            config.leftSwipe,
-            config.undoLeftSwipe,
-            config.rightSwipe,
-            config.undoRightSwipe));
+    if (_displaySplash) {
+      _removeSplash();
+      return new ToDoSplash();
+    } else {
+      return new Scaffold(
+          drawer: new ToDoAppDrawer(
+              config.indicator,
+              config.color,
+              config.brightness,
+              config.changeTheme,
+              config.changeColor,
+              config.emptyTrash),
+          appBar: new AppBar(
+            elevation: 2,
+            title: _getAppTitle(),
+            actions: [
+              config.indicator == 0
+                  ? new IconButton(
+                      icon: new Icon(Icons.create),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          new MaterialPageRoute(
+                              builder: (BuildContext context) {
+                            return new ToDoAppCreate(config.createToDo);
+                          }),
+                        );
+                      })
+                  : new Container(),
+            ],
+          ),
+          body: new ToDoAppBody(
+              config.indicator,
+              config.toDoList,
+              config.toggleDone,
+              config.leftSwipe,
+              config.undoLeftSwipe,
+              config.rightSwipe,
+              config.undoRightSwipe));
+    }
   }
 }
