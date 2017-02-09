9a7f923e2c62d01e6901b9f4012052158eec3c0c
Rainer Wittmann <rainer.wittmann@comsysto.com>
2017-02-09 10:01:03 +0100

    create tododetail class

---
+++ lib/components/todo_app_detail.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ToDoDetail extends StatefulWidget {
+
+  @override
+  State createState() => new ToDoDetailState();
+}
+
+class ToDoDetailState extends State<ToDoDetail> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+  }
+}
+
