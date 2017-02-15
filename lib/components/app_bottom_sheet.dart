46d89fa024b257c634bf175978629382282e7377
Rainer Wittmann <rainer.wittmann@comsysto.com>
2017-02-15 21:56:14 +0100

    create class AppBottomSheet

---
+++ lib/components/app_bottom_sheet.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+
+class AppBottomSheet extends StatefulWidget {
+  BuildContext context;
+  int indicator;
+  var toDo;
+  var leftSwipe;
+  var undoLeftSwipe;
+  var rightSwipe;
+  var undoRightSwipe;
+
+  AppBottomSheet(this.context, this.indicator, this.toDo, this.leftSwipe,
+      this.undoLeftSwipe, this.rightSwipe, this.undoRightSwipe);
+
+  @override
+  State createState() => new AppBottomSheetState();
+}
+
+class AppBottomSheetState extends State<AppBottomSheet> {
+  ThemeData _theme;
+  String _leftText;
+  String _rightText;
+  Icon _leftIcon;
+  Icon _rightIcon;
+
+  @override
+  void initState() {
+    super.initState();
+    switch (config.indicator) {
+      case 0:
+        _leftText = 'ToDo archived';
+        _rightText = 'ToDo deleted';
+        _leftIcon = new Icon(Icons.archive, size: 36.0);
+        _rightIcon = new Icon(Icons.delete, size: 36.0);
+        break;
+      case 1:
+        _leftText = 'ToDo restored';
+        _rightText = 'ToDo deleted';
+        _leftIcon = new Icon(Icons.unarchive, size: 36.0);
+        _rightIcon = new Icon(Icons.delete, size: 36.0);
+        break;
+      default:
+        _leftText = 'ToDo restored';
+        _rightText = 'ToDo deleted';
+        _leftIcon = new Icon(Icons.restore_from_trash, size: 36.0);
+        _rightIcon = new Icon(Icons.delete_forever, size: 36.0);
+        break;
+    }
+    _theme = Theme.of(config.context);
+  }
+
+  void _leftSwipe() {
+    config.leftSwipe(config.toDo, false);
+    Scaffold.of(config.context).showSnackBar(
+          new SnackBar(
+            duration: new Duration(seconds: 2),
+            content: new Text(_leftText),
+            action: new SnackBarAction(
+              label: 'UNDO',
+              onPressed: () {
+                config.undoLeftSwipe(config.toDo, true);
+              },
+            ),
+          ),
+        );
+  }
+
+  void _rightSwipe() {
+    config.rightSwipe(config.toDo, false);
+    Scaffold.of(config.context).showSnackBar(
+          new SnackBar(
+            duration: new Duration(seconds: 2),
+            content: new Text(_rightText),
+            action: new SnackBarAction(
+              label: 'UNDO',
+              onPressed: () {
+                config.undoRightSwipe(config.toDo, true);
+              },
+            ),
+          ),
+        );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      decoration: new BoxDecoration(
+          border: new Border(top: new BorderSide(color: _theme.dividerColor))),
+      child: new Row(
+        children: [
+          new Expanded(
+            flex: 1,
+            child: new DragTarget(
+              builder: (context, a, b) {
+                return _leftIcon;
+              },
+              onAccept: (data) {
+                _leftSwipe();
+                Navigator.pop(context);
+              },
+            ),
+          ),
+          new SizedBox(
+            width: 1.0,
+            height: 48.0,
+            child: new Container(
+              decoration:
+                  new BoxDecoration(backgroundColor: _theme.dividerColor),
+            ),
+          ),
+          new Expanded(
+            flex: 1,
+            child: new DragTarget(
+              builder: (context, a, b) {
+                return _rightIcon;
+              },
+              onAccept: (data) {
+                _rightSwipe();
+                Navigator.pop(context);
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/components/app_todo.dart
+++ lib/components/app_todo.dart
@@ -1,3 +1,4 @@
+import 'package:ToDo/components/app_bottom_sheet.dart';
 import 'package:flutter/material.dart';
 
 class ToDo extends StatefulWidget {
@@ -30,84 +31,15 @@ class ToDo extends StatefulWidget {
 }
 
 class ToDoState extends State<ToDo> {
-  ThemeData _theme;
   bool _expanded;
-  String _leftText;
-  String _rightText;
-  Icon _leftIcon;
-  Icon _rightIcon;
 
   double _boxHeight;
-
-  @override
-  void initState() {
-    super.initState();
-    switch (config.indicator) {
-      case 0:
-        _leftText = 'ToDo archived';
-        _rightText = 'ToDo deleted';
-        _leftIcon = new Icon(Icons.archive, size: 36.0);
-        _rightIcon = new Icon(Icons.delete, size: 36.0);
-        break;
-      case 1:
-        _leftText = 'ToDo restored';
-        _rightText = 'ToDo deleted';
-        _leftIcon = new Icon(Icons.unarchive, size: 36.0);
-        _rightIcon = new Icon(Icons.delete, size: 36.0);
-        break;
-      default:
-        _leftText = 'ToDo restored';
-        _rightText = 'ToDo deleted';
-        _leftIcon = new Icon(Icons.restore_from_trash, size: 36.0);
-        _rightIcon = new Icon(Icons.delete_forever, size: 36.0);
-        break;
-    }
-  }
 
   void _onDragStart() {
     config.toggleExpand(config.toDo, false);
     Scaffold.of(context).showBottomSheet((context) {
-      return new Container(
-        decoration: new BoxDecoration(
-            border:
-                new Border(top: new BorderSide(color: _theme.dividerColor))),
-        child: new Row(
-          children: [
-            new Expanded(
-              flex: 1,
-              child: new DragTarget(
-                builder: (context, a, b) {
-                  return _leftIcon;
-                },
-                onAccept: (data) {
-                  _leftSwipe();
-                  Navigator.pop(context);
-                },
-              ),
-            ),
-            new SizedBox(
-              width: 1.0,
-              height: 48.0,
-              child: new Container(
-                decoration:
-                    new BoxDecoration(backgroundColor: _theme.dividerColor),
-              ),
-            ),
-            new Expanded(
-              flex: 1,
-              child: new DragTarget(
-                builder: (context, a, b) {
-                  return _rightIcon;
-                },
-                onAccept: (data) {
-                  _rightSwipe();
-                  Navigator.pop(context);
-                },
-              ),
-            ),
-          ],
-        ),
-      );
+      return new AppBottomSheet(context, config.indicator, config.toDo, config.leftSwipe,
+          config.undoLeftSwipe, config.rightSwipe, config.undoRightSwipe);
     });
   }
 
@@ -115,44 +47,11 @@ class ToDoState extends State<ToDo> {
     Navigator.pop(context);
   }
 
-  void _leftSwipe() {
-    config.leftSwipe(config.toDo, false);
-    Scaffold.of(context).showSnackBar(
-          new SnackBar(
-            duration: new Duration(seconds: 2),
-            content: new Text(_leftText),
-            action: new SnackBarAction(
-              label: 'UNDO',
-              onPressed: () {
-                config.undoLeftSwipe(config.toDo, true);
-              },
-            ),
-          ),
-        );
-  }
-
-  void _rightSwipe() {
-    config.rightSwipe(config.toDo, false);
-    Scaffold.of(context).showSnackBar(
-          new SnackBar(
-            duration: new Duration(seconds: 2),
-            content: new Text(_rightText),
-            action: new SnackBarAction(
-              label: 'UNDO',
-              onPressed: () {
-                config.undoRightSwipe(config.toDo, true);
-              },
-            ),
-          ),
-        );
-  }
-
   @override
   Widget build(BuildContext context) {
     setState(() {
-      _theme = Theme.of(context);
       _expanded = config.expanded;
-      _boxHeight = _expanded ? 64.0 : 0.0;
+      _boxHeight = _expanded ? 140.0 : 0.0;
     });
     return new Stack(
       key: new ObjectKey(config.toDo),
@@ -206,8 +105,8 @@ class ToDoState extends State<ToDo> {
                   ),
                   title: new Text(config.toDo['title']),
                   subtitle: new Text(config.toDo['subtitle']),
-                  trailing: _expanded ?
-                    new Icon(Icons.keyboard_arrow_up)
+                  trailing: _expanded
+                      ? new Icon(Icons.keyboard_arrow_up)
                       : new Container(),
                   onTap: () {
                     config.toggleExpand(config.toDo, !_expanded);
@@ -215,39 +114,33 @@ class ToDoState extends State<ToDo> {
                 ),
                 new AnimatedContainer(
                   height: _boxHeight,
-                  duration: const Duration(milliseconds: 200),
+                  duration: const Duration(milliseconds: 300),
                   curve: Curves.ease,
-                  child: new Container(
-                          alignment: FractionalOffset.topLeft,
-                          padding:
-                              new EdgeInsets.fromLTRB(24.0, 0.0, 24.0, 12.0),
-                          child: new Text(
-                            config.toDo['description'].length >= 140
-                                ? config.toDo['description'].substring(0, 140) +
-                                    '...'
-                                : config.toDo['description'],
-                            textScaleFactor: 0.9,
-                          ),
+                  padding: new EdgeInsets.fromLTRB(24.0, 0.0, 24.0, 0.0),
+                  child: new Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      new Text(
+                        'Discription:   ' + config.toDo['description'],
+                        textScaleFactor: 0.9,
+                        maxLines: 3,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      new Divider(),
+                      new Text('More Details... need to be added',
+                          textScaleFactor: 0.9),
+                      new Divider(),
+                      new FlatButton(
+                        child: new Row(
+                          children: [
+                            new Icon(Icons.edit),
+                            new Text('   Edit'),
+                          ],
                         ),
-//                      config.indicator == 0
-//                          ? new Container(
-//                              alignment: FractionalOffset.bottomRight,
-//                              padding: new EdgeInsets.only(right: 12.0),
-//                              child: new IconButton(
-//                                icon: new Icon(Icons.edit),
-//                                onPressed: () {
-//                                      Navigator.push(
-//                                        context,
-//                                        new MaterialPageRoute(
-//                                          builder: (context) {
-//                                            return new ToDoDetail();
-//                                          },
-//                                        ),
-//                                      );
-//                                },
-//                              ),
-//                            )
-//                          : new Container(),
+                        onPressed: () {},
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
@@ -267,7 +160,7 @@ class ToDoState extends State<ToDo> {
           builder: (context, a, b) {
             return new AnimatedContainer(
               curve: Curves.ease,
-              duration: const Duration(milliseconds: 200),
+              duration: const Duration(milliseconds: 300),
               height: 60.0 + _boxHeight,
             );
           },
